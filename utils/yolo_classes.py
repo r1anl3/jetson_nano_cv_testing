c7a41f506e97a3e253c4a150f7431f6f39ecaafc
r1anl3 <levu1409@gmail.com>
2024-04-08 12:29:48 +0700

    Update yolo_classes.py

--- utils/yolo_classes.py
+++ utils/yolo_classes.py
@@ -102,7 +102,7 @@ def get_cls_dict(category_num):
     """Get the class ID to name translation dictionary."""
     if category_num == 80:
         return {i: n for i, n in enumerate(COCO_CLASSES_LIST)}
-    # elif category_num == 1:
-    #     return {i: n for i, n in enumerate(MY_CLASSES_LIST)}
+    elif category_num == 1:
+        return {i: n for i, n in enumerate(MY_CLASSES_LIST)}
     else:
         return {i: 'CLS%d' % i for i in range(category_num)}
